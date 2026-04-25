0002a14d512b3c4047382fce5a471e769195a062
Ruslana <shyshkoruslana08@gmail.com>
2026-04-25 22:34:44 +0300

    add data in class Node

--- Node.h
+++ Node.h
@@ -1,10 +1,15 @@
 #ifndef NODE_H
 #define NODE_H
+#include <memory>
+using namespace std;
 
-
+template <typename T>
 class Node {
-
+    T data;
+    shared_ptr<Node<T>> next;
+    weak_ptr<Node<T>> prev;
+    Node(const T& d):data(d),next(nullptr),prev(){}
 };
 
 
-#endif 
+#endif
